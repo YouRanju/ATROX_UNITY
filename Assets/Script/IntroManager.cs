ad6599f1d415e035c72896e6f5def555f3d262ac
YouRanju <dbfkswn0321@naver.com>
2019-06-04 07:03:40 +0900

    Intro modifying

--- Assets/Script/IntroManager.cs
+++ Assets/Script/IntroManager.cs
@@ -2,35 +2,47 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class IntroManager : MonoBehaviour
 {
-    public Sprite[] IntroImg;
+    public GameObject[] IntroImg;
+    public Image fade;
     private float rdt;
     private int cnt = 0;
+
+    float fades = 0.0f;
+    float time = 0;
 
     // Start is called before the first frame update
     void Start()
     {
-        gameObject.GetComponent<SpriteRenderer>().sprite = IntroImg[0];
+        
     }
 
     // Update is called once per frame
     void Update()
     {
         rdt += Time.deltaTime;
+        time += Time.deltaTime;
 
-        if (rdt > 2f)
+        if (fades < 1.0f && time >= 0.1f)
         {
+            fades += time;
+            fade.color = new Color(0, 0, 0, fades);
+            time = 0;
+        }
+        else if (fades >= 1.0f)
+        {
+            SceneManager.LoadScene("Main");
+        }
+
+        if (rdt > 2f&& cnt < IntroImg.Length)
+        {
+            IntroImg[cnt].SetActive(true);
             cnt++;
 
-            gameObject.GetComponent<SpriteRenderer>().sprite = IntroImg[cnt];
             rdt = 0;
-
-            if (cnt >= IntroImg.Length - 1)
-            {
-                SceneManager.LoadScene("Main");
-            }
         }
     }
 }
---
+++ Assets/Script/IntroMove.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IntroMove : MonoBehaviour
+{
+    public GameObject[] IntroImg;
+    private float rdt;
+    private int cnt = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for(int i = 0; i < IntroImg.Length; i++)
+        {
+            if(i%2 == 0)
+            {
+                Vector2 vec = new Vector2(0, -2);
+                IntroImg[i].GetComponent<Transform>().position = vec; 
+            } else
+            {
+                Vector2 vec = new Vector2(0, 2);
+                IntroImg[i].GetComponent<Transform>().position = vec;
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (cnt < IntroImg.Length)
+        {
+            if (IntroImg[cnt].activeInHierarchy == true && cnt < IntroImg.Length)
+            {
+                rdt += Time.deltaTime;
+
+                if (cnt % 2 == 0)
+                {
+                    IntroImg[cnt].GetComponent<Rigidbody2D>().velocity = (Vector2.up * 2f);
+
+                }
+                else
+                {
+                    IntroImg[cnt].GetComponent<Rigidbody2D>().velocity = (Vector2.down * 2f);
+
+                }
+
+                if (rdt > 1f)
+                {
+                    IntroImg[cnt].GetComponent<Rigidbody2D>().velocity = (Vector2.zero);
+                    rdt = 0;
+                    cnt++;
+                }
+
+
+            }
+        }
+        
+    }
+}
