fde70abbd358618f6fbf158d06a19a4c20f39803
YouRanju <dbfkswn0321@naver.com>
2019-06-04 19:54:19 +0900

    Modifying Title

---
+++ Assets/Script/Starlight.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Starlight : MonoBehaviour
+{
+    public GameObject logo;
+
+    private float alpha;
+    private float time;
+    private bool isAl = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.x > 1.6)
+        {
+            GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+
+            time += Time.deltaTime;
+
+            if (alpha < 8.0f && time >= 0.1f && !isAl)
+            {
+                alpha += time * 12;
+                time = 0;
+            } else if( time >= 0.1f)
+            {
+                isAl = true;
+                alpha -= time * 12;
+                if (alpha < 0)
+                {
+                    isAl = false;
+                }
+
+                time = 0;
+            }
+            GetComponent<Light>().intensity = alpha;
+        }
+        else
+        {
+            GetComponent<Rigidbody2D>().velocity = Vector2.right * 5;
+        }
+    }
+}
---
+++ Assets/Script/TitleMeteorMove.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleMeteorMove : MonoBehaviour
+{
+    private float fdt;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        fdt += Time.deltaTime;
+
+        if(fdt > 1f)
+        {
+            GetComponent<Rigidbody2D>().velocity = (Vector2.up * 0.3f);
+            
+        }
+        if (fdt > (1.5f) )
+        {
+            GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+            fdt = 0;
+        }
+    }
+}
